6a7ed7b52d16a06337e8e312e2b7fe1dddca2aa8
Izak Hearn <izakhearn@gmail.com>
2024-02-14 09:50:08 +0200

    Made Custom Quiz Playable

--- src/js/play.js
+++ src/js/play.js
@@ -1,7 +1,37 @@
 import Quiz from "./playModule.mjs";
-import { showCategories } from "./playModule.mjs";
+import { CustomQuiz, showCategories } from "./playModule.mjs";
 
-showCategories();
+const customQuiz = document.querySelector(".custom__quiz");
+const normalQuiz = document.querySelector(".normal__quiz");
+
+customQuiz.addEventListener("click", () => {
+  document.querySelector("#custom-quiz").classList.toggle("hidden");
+  document.querySelector("#normal-quiz").classList.add("hidden");
+  const startButton = document.querySelector(".start__custom__quiz");
+  startButton.addEventListener("click", () => {
+    const quizName = document.querySelector(".quiz__name").value;
+    document.querySelector(".quiz__container").innerHTML = "";
+    const quiz = new CustomQuiz(quizName);
+    quiz.init();
+    startButton.setAttribute("disabled", true);
+    let time = 10;
+    const interval = setInterval(() => {
+      startButton.innerHTML = `Please wait ${time} seconds`;
+      time--;
+      if (time < 0) {
+        clearInterval(interval);
+        startButton.removeAttribute("disabled");
+        startButton.innerHTML = "Start Quiz";
+      }
+    }, 1000);
+  });
+}
+);
+
+normalQuiz.addEventListener("click", () => {
+  document.querySelector("#normal-quiz").classList.toggle("hidden");
+  document.querySelector("#custom-quiz").classList.add("hidden");
+  showCategories();
 
 const startButton = document.querySelector(".start__quiz");
 startButton.addEventListener("click", () => {
@@ -24,3 +54,6 @@ startButton.addEventListener("click", () => {
     }
   }, 1000);
 });
+}
+);
+
