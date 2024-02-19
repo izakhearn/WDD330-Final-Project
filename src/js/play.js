871ff85a906ece7fbb61c6ecf15213f2bd6c88b7
Izak Hearn <izakhearn@gmail.com>
2024-02-19 13:44:16 +0200

    Add Team Mode and Fireworks on Game End

--- src/js/play.js
+++ src/js/play.js
@@ -1,5 +1,5 @@
 import Quiz from "./playModule.mjs";
-import { CustomQuiz, showCategories } from "./playModule.mjs";
+import { CustomQuiz, showCategories, TeamQuiz } from "./playModule.mjs";
 
 const customQuiz = document.querySelector(".custom__quiz");
 const normalQuiz = document.querySelector(".normal__quiz");
@@ -13,6 +13,8 @@ customQuiz.addEventListener("click", () => {
     document.querySelector(".quiz__container").innerHTML = "";
     const quiz = new CustomQuiz(quizName);
     quiz.init();
+    const quizType = document.querySelector(".quiz__type");
+    quizType.classList.add("hidden");
     startButton.setAttribute("disabled", true);
     let time = 10;
     const interval = setInterval(() => {
@@ -25,35 +27,39 @@ customQuiz.addEventListener("click", () => {
       }
     }, 1000);
   });
-}
-);
+});
 
 normalQuiz.addEventListener("click", () => {
   document.querySelector("#normal-quiz").classList.toggle("hidden");
   document.querySelector("#custom-quiz").classList.add("hidden");
   showCategories();
 
-const startButton = document.querySelector(".start__quiz");
-startButton.addEventListener("click", () => {
-  const category = document.querySelector("#category").value;
-  const difficulty = document.querySelector("#difficulty").value;
-  const amount = document.querySelector("#amount").value;
-  document.querySelector(".quiz__container").innerHTML = "";
-  const quiz = new Quiz(category, difficulty, amount);
-  quiz.init();
-  //   Disable quiz button for 10s to prevent api time out
-  startButton.setAttribute("disabled", true);
-  let time = 10;
-  const interval = setInterval(() => {
-    startButton.innerHTML = `Please wait ${time} seconds`;
-    time--;
-    if (time < 0) {
-      clearInterval(interval);
-      startButton.removeAttribute("disabled");
-      startButton.innerHTML = "Start Quiz";
+  const startButton = document.querySelector(".start__quiz");
+  startButton.addEventListener("click", () => {
+    const category = document.querySelector("#category").value;
+    const difficulty = document.querySelector("#difficulty").value;
+    const amount = document.querySelector("#amount").value;
+    document.querySelector(".quiz__container").innerHTML = "";
+    if (document.querySelector("#team-mode").checked) {
+      const quiz = new TeamQuiz(category, difficulty, amount);
+      quiz.init();
+    } else {
+      const quiz = new Quiz(category, difficulty, amount);
+      quiz.init();
     }
-  }, 1000);
+    const quizType = document.querySelector(".quiz__type");
+    quizType.classList.add("hidden");
+    //   Disable quiz button for 10s to prevent api time out
+    startButton.setAttribute("disabled", true);
+    let time = 10;
+    const interval = setInterval(() => {
+      startButton.innerHTML = `Please wait ${time} seconds`;
+      time--;
+      if (time < 0) {
+        clearInterval(interval);
+        startButton.removeAttribute("disabled");
+        startButton.innerHTML = "Start Quiz";
+      }
+    }, 1000);
+  });
 });
-}
-);
-
